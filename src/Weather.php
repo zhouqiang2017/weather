cdcbdb7530efe015f3f5f78c4ea293da95896b4e
zhouqiang2017 <443682811@qq.com>
2019-04-27 19:15:19 +0800

    Add travis ci config file.

--- src/Weather.php
+++ src/Weather.php
@@ -30,14 +30,26 @@ class Weather {
         $this->guzzleOptions = $options;
     }
 
+
+    public function getLiveWeather($city, $format = 'json')
+    {
+        return $this->getWeather($city, 'base', $format);
+    }
+
+    public function getForecastsWeather($city, $format = 'json')
+    {
+        return $this->getWeather($city, 'all', $format);
+    }
+
     public function getWeather($city, $type = 'base', $format = 'json')
     {
         $url = 'https://restapi.amap.com/v3/weather/weatherInfo';
+        $types = ['base','all'];
         // 1. 对 `$format` 与 `$type` 参数进行检查，不在范围内的抛出异常。
         if ( !\in_array(\strtolower($format), ['xml', 'json'])) {
             throw new InvalidArgumentException('Invalid response format: ' . $format);
         }
-        if ( !\in_array(\strtolower($type), ['base', 'all'])) {
+        if ( !\in_array(\strtolower($type), $types)) {
             throw new InvalidArgumentException('Invalid type value(base/all): ' . $type);
         }
         // 2. 封装 query 参数，并对空值进行过滤。
